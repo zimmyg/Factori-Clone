8d0c430a3f1e58048cdc72dee27ff6c715a5cdd5
Tim <Tim@Z-BATTLESTATION>
2016-06-23 00:56:19 +1000

    Set up the project and used the LazyFoo tutorial to test that it works

---
+++ Factori-Clone/Factori-Clone/main.cpp
@@ -0,0 +1,113 @@
+//Using SDL and standard IO
+#include <SDL/SDL.h>
+#include <stdio.h>
+
+
+bool init(); //Starts up SDL and creates window
+bool loadMedia(); //Loads media
+void close(); //Frees media and shuts down SDL
+
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+//The window we'll be rendering to
+SDL_Window* gWindow = NULL;
+
+//The surface contained by the window
+SDL_Surface* gScreenSurface = NULL;
+
+// Image we're loading and showing on screen
+SDL_Surface* gHelloWorld = NULL;
+
+int main(int argc, char* args[])
+{
+	// Start up SDL and create window
+	if ( !init() )
+	{
+		printf("Failed to initialize!\n");
+	}
+	else
+	{
+		if ( !loadMedia() )
+		{
+			printf("Failed to load media!\n");
+		}
+		else
+		{
+			// Apply the image
+			SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
+
+			// Update the surface
+			SDL_UpdateWindowSurface( gWindow );
+
+			// Wait five seconds 
+			SDL_Delay( 5000 );
+		}
+	}
+
+	// Free resources and close SDL
+	close();
+
+	return 0;
+}
+
+bool init()
+{
+	bool success = true;
+
+	// Initailise SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("SDL could not initialise! SDL_Error: %s\n", SDL_GetError());
+		success = false;
+	}
+	else
+	{
+		//Create window
+		gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (gWindow == NULL)
+		{
+			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+			success = false;
+		}
+		else
+		{
+			//Get window surface
+			gScreenSurface = SDL_GetWindowSurface(gWindow);
+		}
+	}
+
+	return success;
+}
+
+bool loadMedia()
+{
+	//Loading success flag
+	bool success = true;
+
+	//Load splash image
+	gHelloWorld = SDL_LoadBMP("res/testImage.bmp");
+	if (gHelloWorld == NULL)
+	{
+		printf("Unable to load image %s! SDL Error: %s\n", "testImage.bmp", SDL_GetError());
+		success = false;
+	}
+
+	return success;
+}
+
+void close()
+{
+	//Deallocate surface
+	SDL_FreeSurface(gHelloWorld);
+	gHelloWorld = NULL;
+
+	//Destroy window
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+
+	//Quit SDL subsystems
+	SDL_Quit();
+}
